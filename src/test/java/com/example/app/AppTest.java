5f724ae0b33fb61652f162d5774a5dfa02a5cdc9
Monish-vit <monishwar.s2023@vitstudent.ac.in>
2026-04-13 10:38:02 +0530

    Add unit tests for Voter eligibility checks

---
+++ src/test/java/com/example/app/AppTest.java
@@ -0,0 +1,25 @@
+package com.voting.app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    
+    @Test
+    public void testEligibleVoter() {
+        Voter v = new Voter("John", 20, "Indian", "ID123", true);
+        assertEquals("Eligible to Vote", v.checkEligibility());
+    }
+
+    @Test
+    public void testUnderageVoter() {
+        Voter v = new Voter("Sam", 16, "Indian", "ID456", true);
+        assertEquals("Ineligible: Underage (Must be 18+)", v.checkEligibility());
+    }
+
+    @Test
+    public void testNonCitizen() {
+        Voter v = new Voter("Ali", 25, "Foreign", "ID789", true);
+        assertEquals("Ineligible: Not an Indian citizen", v.checkEligibility());
+    }
+}
